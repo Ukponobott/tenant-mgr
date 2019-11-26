ea3e755b5eb0d2817363d44cf938ae533619dd75
ukponoobott <legendsergio@gmail.com>
2019-11-26 10:30:34 +0100

    new commit

--- tenant_management/tenant_management/doctype/authorised_tenant/authorised_tenant.py
+++ tenant_management/tenant_management/doctype/authorised_tenant/authorised_tenant.py
@@ -3,8 +3,10 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class AuthorisedTenant(Document):
-	pass
+	def validate(self):
+		if property_name != confirm_property:
+			frappe.throw(_("Property does not match"))
